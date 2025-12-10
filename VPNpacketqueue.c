ac120e128d1d08e537604a10eebe2b85b2666252
KCJ07 <kjohanson321@gmail.com>
2025-12-10 12:44:04 -0600

    queue initializer

--- VPNpacketqueue.c
+++ VPNpacketqueue.c
@@ -8,10 +8,10 @@
 // structure for queue
 typedef struct packetQueue {
     packet_t **packets; // array of pointers to packets
-    int capacity; // maximumum queue size
-    int size; // current ammount of packets
-    int head;
-    int tail;
+    int maxCapacity; // maximumum queue size
+    int curSize; // current ammount of packets
+    int head; // head index
+    int tail; // tail index
     pthread_mutex_t mutex; // thread for accessing queue
     pthread_cond_t notEmpty;
     pthread_cond_t notFull;
@@ -22,6 +22,40 @@ typedef struct packetQueue {
 //initalize multi thread packet queue
 int queueInit(packetQueue_t **queue, int capacity, int queueID) {
 
+    // malloc space for struct
+    packetQueue_t *queueSpace= malloc(sizeof(packetQueue_t))
+    if (!queueSpace) { // checks if malloc failed
+        perror("Failed to Malloc"); 
+        return;
+    }
+
+    //malloc space for packet pointer array
+    queueSpace->packets = malloc(sizeof (packet_t *) * capacity);
+    if (!queueSpace->packets) { // checks if malloc failed
+        perror("Failed to Malloc"); 
+        return;
+    }
+
+    // sets array of pointers to packets to all 0 
+    memset(q->packets, 0, sizeof(packet_t *) * capacity);
+
+    //initialze rest of struct
+    queueSpace->maxCapacity = capacity;
+    queueSpace->size = 0;
+    queueSpace->head = 0;
+    queueSpace->tail = 0;
+    queueSpace->queueID = queueID; 
+
+    pthread_mutex_init(&queueSpace->mutex, NULL);     // Mutex for exclusive queue access
+
+    // cond_init are: A condition (short for "condition variable") is a synchronization
+    // device that allows threads to suspend execution and relinquish the
+    // processors until some predicate on shared data is satisfied.
+    pthread_cond_init(&queueSpace->notEmpty, NULL);  // variable for when queue has data
+    pthread_cond_init(&queueSpace->notFull, NULL);   // variable for when queue has space
+
+    *queue = queueSpace;
+    return 0;
 }
 
 // push for our queue
